f31d532cf5e35b1a093f1500b32648195c435549
John Benedict Aguirre <144007511+jobenoidz@users.noreply.github.com>
2025-07-17 15:31:47 +0800

    disabled dark mode

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark' => ($appearance ?? 'system') == 'dark'])>
+<!-- <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @class(['dark' => ($appearance ?? 'system') == 'dark'])> -->
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+</html>
 
 <head>
     <meta charset="utf-8">
